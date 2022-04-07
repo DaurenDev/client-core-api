ce3f9c93c559070034220a5e5886832dffeacc0b
Dauren <dauren141001@gmail.com>
2022-04-07 13:23:31 +0600

    Initial commit

---
+++ src/main/java/kz/dar/academy/backend/service/ClientService.java
@@ -0,0 +1,17 @@
+package kz.dar.academy.backend.service;
+
+import kz.dar.academy.backend.model.ClientModel;
+
+import java.util.List;
+
+public interface ClientService {
+    void createClient(ClientModel clientModel);
+
+    List<ClientModel> getAllClients();
+
+    ClientModel getClientById(String clientId);
+
+    void updateClientById(String clientId, ClientModel clientModel);
+
+    void deleteClientById(String clientId);
+}
---
+++ src/main/java/kz/dar/academy/backend/service/ClientServiceImplementation.java
@@ -0,0 +1,54 @@
+package kz.dar.academy.backend.service;
+
+import kz.dar.academy.backend.model.ClientModel;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ClientServiceImplementation implements ClientService{
+    private static final HashMap<String, ClientModel> clientMap = new HashMap<>();
+
+
+    static {
+        ClientModel clientModel1 = new ClientModel(UUID.randomUUID().toString(), "Ivan", "Petrov",
+                "DAR", "Engineer", "example1@gmail.com", 300000);
+        ClientModel clientModel2 = new ClientModel(UUID.randomUUID().toString(), "Maria", "Petrova",
+                "DAR", "Engineer", "example2@gmail.com", 200000);
+        ClientModel clientModel3 = new ClientModel(UUID.randomUUID().toString(), "Ivan", "Smith",
+                "DAR", "Engineer", "example3@gmail.com", 100000);
+        clientMap.put(clientModel1.getClientId(), clientModel1);
+        clientMap.put(clientModel2.getClientId(), clientModel2);
+        clientMap.put(clientModel3.getClientId(), clientModel3);
+
+    }
+
+    @Override
+    public void createClient(ClientModel clientModel) {
+        clientModel.setClientId(UUID.randomUUID().toString());
+    }
+
+    @Override
+    public List<ClientModel> getAllClients() {
+        return new ArrayList<>(clientMap.values());
+    }
+
+    @Override
+    public ClientModel getClientById(String clientId) {
+        return clientMap.get(clientId);
+    }
+
+    @Override
+    public void updateClientById(String clientId, ClientModel clientModel) {
+        clientModel.setClientId(clientId);
+        clientMap.put(clientId, clientModel);
+    }
+
+    @Override
+    public void deleteClientById(String clientId) {
+        clientMap.remove(clientId);
+    }
+}
